79803e06bead07aabd3cfd80b6ba318e00b43515
Yarin Ronel <y3arin@gmail.com>
2019-04-21 14:32:59 +0300

    feat: Basic landing page and styles

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,24 +1,23 @@
-import { BrowserModule } from '@angular/platform-browser';
-import { NgModule } from '@angular/core';
+import { BrowserModule } from "@angular/platform-browser";
+import { NgModule } from "@angular/core";
+import { MatButtonModule, MatCardModule } from "@angular/material";
 
-import { AppRoutingModule } from './app-routing.module';
-import { AppComponent } from './app.component';
-import { ShiftsInputComponent } from './shifts-input/shifts-input.component';
-import { ShiftsResultComponent } from './shifts-result/shifts-result.component';
-import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+import { AppRoutingModule } from "./app-routing.module";
+import { AppComponent } from "./app.component";
+import { ShiftsInputComponent } from "./shifts-input/shifts-input.component";
+import { ShiftsResultComponent } from "./shifts-result/shifts-result.component";
+import { BrowserAnimationsModule } from "@angular/platform-browser/animations";
 
 @NgModule({
-  declarations: [
-    AppComponent,
-    ShiftsInputComponent,
-    ShiftsResultComponent
-  ],
+  declarations: [AppComponent, ShiftsInputComponent, ShiftsResultComponent],
   imports: [
     BrowserModule,
     AppRoutingModule,
-    BrowserAnimationsModule
+    BrowserAnimationsModule,
+    MatCardModule,
+    MatButtonModule
   ],
   providers: [],
   bootstrap: [AppComponent]
 })
-export class AppModule { }
+export class AppModule {}
