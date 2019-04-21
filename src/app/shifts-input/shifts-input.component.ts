342fabcbcb4c7ecef7e2cf8ad016d0f640da9d00
Bar Goldinfeld <bargoldinfeld@gmail.com>
2019-04-21 18:23:42 +0300

    shifts-input works

--- src/app/shifts-input/shifts-input.component.ts
+++ src/app/shifts-input/shifts-input.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { FormBuilder, FormControl, FormGroup, Validators } from '@angular/forms';
 
 @Component({
   selector: 'app-shifts-input',
@@ -6,14 +7,69 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./shifts-input.component.less']
 })
 export class ShiftsInputComponent implements OnInit {
-  numberOfWorkers = 1;
-  numberOfDays = 1;
-  numberOfShifts = 1;
-  numberOfNecessaryWorkersPerShift = 1;
+  numberOfWorkers = 7;
+  numberOfDays = 7;
+  numberOfShifts = 3;
+  numberOfNecessaryWorkersPerShift = 2;
 
-  constructor() {
+  shiftsInputForm: FormGroup;
+  // shiftsInputForm.getRawValue()[
+  //   getShiftString(worker, (shift / numberOfWorkers), shift & numberOfWorkers)
+  //   ]
+
+  availabilities = [
+    {value: 1000, icon: 'check_circle_outline', color: 'green'},
+    {value: 100, icon: 'help_outline', color: 'faded-green'},
+    {value: 10, icon: 'help_outline', color: 'orange'},
+    {value: 1, icon: 'highlight_off', color: 'red'}
+  ];
+
+  days = [
+    'Sunday',
+    'Monday',
+    'Tuesday',
+    'Wednesday',
+    'Thursday',
+    'Friday',
+    'Saturday'
+  ];
+
+  constructor(private formBuilder: FormBuilder) {
+    this.rebuildForm();
   }
 
   ngOnInit() {
+  }
+
+  public rebuildForm() {
+    this.shiftsInputForm = this.formBuilder.group({
+      numberOfWorkers: [this.numberOfWorkers, Validators.compose([Validators.min(1), Validators.required])],
+      numberOfDays: [this.numberOfDays, Validators.compose([Validators.min(1), Validators.max(7), Validators.required])],
+      numberOfShifts: [this.numberOfShifts, Validators.compose([Validators.min(1), Validators.max(3), Validators.required])],
+      numberOfNecessaryWorkersPerShift: [this.numberOfNecessaryWorkersPerShift, Validators.compose([Validators.min(1), Validators.required])]
+    });
+
+    for (let i = 0; i < this.numberOfWorkers; i++) {
+      for (let j = 0; j < this.numberOfDays; j++) {
+        for (let k = 0; k < this.numberOfShifts; k++) {
+          console.log(`shift-${i}-${j}-${k}`);
+          this.shiftsInputForm.registerControl(`shift-${i}-${j}-${k}`, new FormControl(
+            this.availabilities[0],
+            Validators.required
+          ));
+        }
+      }
+    }
+  }
+
+  getCurrentValue(worker, relativeShift) {
+    return this.shiftsInputForm.getRawValue()[this.getShiftString(worker, relativeShift)];
+  }
+
+  getShiftString(worker, relativeShift) {
+    const day = (relativeShift / this.numberOfShifts);
+    const shift = relativeShift % this.numberOfShifts;
+
+    return `shift-${worker}-${Math.floor(day)}-${shift}`;
   }
 }
